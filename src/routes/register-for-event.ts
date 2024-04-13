22d0c7d74b2fa4ce1d9c0be2d27ec688414de370
Vitor <vitor.reis@crmservices.com.br>
2024-04-12 22:26:33 -0300

    first commit

---
+++ src/routes/register-for-event.ts
@@ -0,0 +1,77 @@
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { FastifyInstance } from "fastify";
+import { prisma } from "../lib/prisma";
+import { BadRequest } from "./_errors/bad-request";
+
+export async function registerForEvent(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/events/:eventId/attendees",
+    {
+      schema: {
+        summary: "Register an attendee for an event",
+        tags: ["attendees"],
+        body: z.object({
+          name: z.string().min(4),
+          email: z.string().email(),
+        }),
+        params: z.object({
+          eventId: z.string().uuid(),
+        }),
+        response: {
+          201: z.object({
+            attendeeId: z.number(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { eventId } = request.params;
+      const { name, email } = request.body;
+
+      const attendeeFromEmail = await prisma.attendee.findUnique({
+        where: {
+          eventId_email: {
+            email,
+            eventId,
+          },
+        },
+      });
+
+      if (attendeeFromEmail !== null) {
+        throw new BadRequest("Attendee already registered for this event");
+      }
+
+      const [event, amountOfAttendeesForEvent] = await Promise.all([
+        prisma.event.findUnique({
+          where: {
+            id: eventId,
+          },
+        }),
+
+        prisma.attendee.count({
+          where: {
+            eventId,
+          },
+        }),
+      ]);
+
+      if (
+        event?.maximumAttendees &&
+        amountOfAttendeesForEvent >= event?.maximumAttendees
+      ) {
+        throw new BadRequest("Event is full");
+      }
+
+      const attendee = await prisma.attendee.create({
+        data: {
+          name,
+          email,
+          eventId,
+        },
+      });
+
+      return reply.status(201).send({ attendeeId: attendee.id });
+    }
+  );
+}
---
+++ src/server.ts
@@ -0,0 +1,55 @@
+import fastify from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  jsonSchemaTransform,
+} from "fastify-type-provider-zod";
+import { createEvent } from "./routes/create-event";
+import { registerForEvent } from "./routes/register-for-event";
+import { getEvent } from "./routes/get-event";
+import { getAttendeeBadge } from "./routes/get-attendee-badge";
+import { checkIn } from "./routes/check-in";
+import { getEventAttendees } from "./routes/get-event-attendees";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+import { errorHandler } from "./errorHandler";
+import fastifyCors from "@fastify/cors";
+
+const app = fastify();
+
+app.register(fastifyCors, {
+  origin: "*",
+});
+
+app.register(fastifySwagger, {
+  swagger: {
+    consumes: ["application/json"],
+    produces: ["application/json"],
+    info: {
+      title: "Pass.in",
+      description: "Event management API",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+});
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createEvent);
+app.register(registerForEvent);
+app.register(getEvent);
+app.register(getAttendeeBadge);
+app.register(checkIn);
+app.register(getEventAttendees);
+
+app.setErrorHandler(errorHandler);
+
+app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
+  console.log("HTTP server running!");
+});
